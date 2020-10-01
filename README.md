52b47906cd3a1d208af56e0a0db708c869521b30
fenoloftaleina <byldominik+fenoloftaleina@gmail.com>
2020-10-01 21:36:42 +0200

    Do kawusia.

---
+++ README.md
@@ -0,0 +1,3 @@
+# kawusia
+
+Serves images from https://pastebin.com/9SFTX93k for a slack bot sort of thing.
---
+++ src/kawusia/core.clj
@@ -0,0 +1,22 @@
+(ns kawusia.core
+  (:require
+    [ring.adapter.jetty :as jetty]
+    [clj-http.client :as http]
+    [clojure.string :as cstr]))
+
+(defn handler [request]
+  (-> "https://pastebin.com/raw/9SFTX93k"
+      http/get
+      :body
+      (cstr/split #"\r\n")
+      rand-nth
+      (http/get {:as :byte-array})))
+
+(defn -main [& [port]]
+  (jetty/run-jetty #'handler {:port (or port 80) :join? false}))
+
+
+(comment
+  (defonce server (-main 8080))
+
+  )
