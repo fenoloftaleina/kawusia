22441ce4bbaec37679c1bc85734552f068ed30f1
fenoloftaleina <byldominik+fenoloftaleina@gmail.com>
2020-10-01 21:40:00 +0200

    Have a min lein version because of heroku.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :min-lein-version "2.9.1"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [ring "1.8.1"]
                  [clj-http "3.10.3"]]
